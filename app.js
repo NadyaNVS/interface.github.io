ef4ac5109cb4a26f88f0ddc6a1446cf2bfab9af1
NadyaNVS <105170990+NadyaNVS@users.noreply.github.com>
2024-06-12 19:43:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 
 const userName = document.getElementById("user-info");
-userName.innerHTML = `Hello, ${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name} (@${tg.initDataUnsafe.user.username})!`;
+userName.innerHTML = `${tg.initDataUnsafe.user.username}`;
